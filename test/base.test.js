e7061b32c5721edec10711406e347c03937adc4c
Jansen <jan53n@outlook.in>
2024-10-26 09:42:47 +0530

    cleanup

---
+++ test/base.test.js
@@ -0,0 +1,8 @@
+import { parse } from "../index.js";
+import { describe, it } from "node:test";
+
+describe("ISO 8601 Duration Parser", () => {
+  it("should parse a duration with only years", () => {
+    //
+  });
+});
