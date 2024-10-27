928b3a3e541b694a782feb4ae7583e2a3bdc6c58
Jansen <jan53n@outlook.in>
2024-10-27 11:50:48 +0530

    Add a naive serializer (works most of the time ;))

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 import { parse as _parse } from "./generated/parser.js";
 
 /**
- * @param {string} payload 
+ * @typedef {Record<string, string[]|null>} CSPParserResult
+ */
+
+/**
+ * parse CSP string into object
+ * @param {string} payload
+ * @return {CSPParserResult}
  */
 export function parse(payload) {
     return _parse(payload);
+}
+
+/**
+ * serialize parse result into a valid CSP string
+ * @param {CSPParserResult} payload
+ * @return {string}
+ */
+export function serialize(payload) {
+    let serialized = "";
+
+    for (const name in payload) {
+        if (Object.prototype.hasOwnProperty.call(payload, name)) {
+            const rules = payload[name]?.join(" ");
+            serialized += `${name}${rules ? " " + rules : ""};`;
+        }
+    }
+
+    return serialized;
 }
