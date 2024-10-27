f03aed3394499b57c8c13ecb82f8c965c59bede7
Jansen <jan53n@outlook.in>
2024-10-27 13:03:01 +0530

    Add proper log messages

--- bin/build.js
+++ bin/build.js
@@ -4,8 +4,10 @@ import { join } from "path";
 import { watch } from 'node:fs';
 import { argv } from "process";
 
+const GRAMMAR_PATH = "grammar";
+
 const START_GRAMMAR_FILE = join(
-  "grammar",
+  GRAMMAR_PATH,
   "syntax.pegjs",
 );
 
@@ -25,13 +27,17 @@ function build() {
 
   fs.mkdirSync(WRITE_PATH, { recursive: true });
   fs.writeFileSync(join(WRITE_PATH, "parser.js"), parserSource);
+
+  console.info("Build complete, files written under", WRITE_PATH);
 }
 
 build();
 
 if (argv[2] === "watch") {
-  watch('./grammar', { encoding: 'buffer' }, (eventType, filename) => {
-    console.info((new Date()).getTime(), `rebuilding, ${filename} changed. (${eventType})`);
+  console.info("Watching for grammar file changes...");
+
+  watch(GRAMMAR_PATH, { encoding: 'buffer' }, (eventType, filename) => {
+    console.info(`rebuilding, ${filename} changed. (${eventType})`);
 
     try {
       build();
