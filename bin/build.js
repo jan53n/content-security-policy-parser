126f4d8ab5285aad069a1441046f318c06a8413a
Jansen <jan53n@outlook.in>
2024-10-27 10:03:10 +0530

    Add optional watcher on /grammar files

--- bin/build.js
+++ bin/build.js
@@ -1,25 +1,43 @@
 import peggy from "peggy";
 import fs from "fs";
 import { join } from "path";
+import { watch } from 'node:fs';
+import { argv } from "process";
 
-const { NODE_ENV } = process.env;
-const CUSTOM_BUILD_PATH = NODE_ENV || "";
-const START_GRAMMAR_FILE = join(
-  "grammar",
-  "syntax.pegjs",
-);
+function build() {
+  const { NODE_ENV } = process.env;
+  const CUSTOM_BUILD_PATH = NODE_ENV || "";
+  const START_GRAMMAR_FILE = join(
+    "grammar",
+    "syntax.pegjs",
+  );
 
-const WRITE_PATH = join("generated", CUSTOM_BUILD_PATH);
-const sourceText = fs.readFileSync(START_GRAMMAR_FILE).toString();
+  const WRITE_PATH = join("generated", CUSTOM_BUILD_PATH);
+  const sourceText = fs.readFileSync(START_GRAMMAR_FILE).toString();
 
-/** @type {import("peggy").SourceBuildOptions}  */
-const buildConfig = {
-  output: "source",
-  cache: true,
-  format: "es",
-};
+  /** @type {import("peggy").SourceBuildOptions}  */
+  const buildConfig = {
+    output: "source",
+    cache: true,
+    format: "es",
+  };
 
-const parserSource = peggy.generate(sourceText, buildConfig);
+  const parserSource = peggy.generate(sourceText, buildConfig);
 
-fs.mkdirSync(WRITE_PATH, { recursive: true });
-fs.writeFileSync(join(WRITE_PATH, "parser.js"), parserSource);
+  fs.mkdirSync(WRITE_PATH, { recursive: true });
+  fs.writeFileSync(join(WRITE_PATH, "parser.js"), parserSource);
+}
+
+if (argv[2] === "watch") {
+  watch('./grammar', { encoding: 'buffer' }, (eventType, filename) => {
+    console.info((new Date()).getTime(), `rebuilding, ${filename} changed. (${eventType})`);
+
+    try {
+      build();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+} else {
+  build();
+}
