d61368ecc948169b8eb42d6ca744d5570809eca9
Jansen <jan53n@outlook.in>
2024-10-28 09:36:39 +0530

    cleanup build script

--- bin/build.js
+++ bin/build.js
@@ -5,16 +5,24 @@ import { watch } from 'node:fs';
 import { argv } from "process";
 
 const GRAMMAR_PATH = "grammar";
-
 const START_GRAMMAR_FILE = join(
   GRAMMAR_PATH,
   "syntax.pegjs",
 );
-
 const WRITE_PATH = "generated";
+const WATCH_MODE_PREFIX = "watch";
+const WATCH_MODE = argv[2] || "";
 
 function build() {
   const sourceText = fs.readFileSync(START_GRAMMAR_FILE).toString();
+
+  /** @type {import("peggy").SourceText[]} */
+  const input = [
+    {
+      source: START_GRAMMAR_FILE,
+      text: sourceText,
+    }
+  ];
 
   /** @type {import("peggy").SourceBuildOptions}  */
   const buildConfig = {
@@ -23,7 +31,17 @@ function build() {
     format: "es",
   };
 
-  const parserSource = peggy.generate(sourceText, buildConfig);
+  let parserSource;
+
+  try {
+    parserSource = peggy.generate(input, buildConfig);
+  } catch (error) {
+    if (WATCH_MODE) {
+      return console.error(error);
+    } else {
+      throw error;
+    }
+  }
 
   fs.mkdirSync(WRITE_PATH, { recursive: true });
   fs.writeFileSync(join(WRITE_PATH, "parser.js"), parserSource);
@@ -33,16 +51,11 @@ function build() {
 
 build();
 
-if (argv[2] === "watch") {
+if (WATCH_MODE.startsWith(WATCH_MODE_PREFIX)) {
   console.info("Watching for grammar file changes...");
 
   watch(GRAMMAR_PATH, { encoding: 'buffer' }, (eventType, filename) => {
     console.info(`rebuilding, ${filename} changed. (${eventType})`);
-
-    try {
-      build();
-    } catch (error) {
-      console.error(error);
-    }
+    build();
   });
 }
